014d43d54a18a6c2edcf23b6d38caa48c0074c11
HFBS <d@hfbs.it>
2026-07-16 17:05:26 +0000

    Update logo to H-FARM Business School tractor icon

--- src/data/corsiForYear.js
+++
@@ -1,7 +0,0 @@
-import { state } from '../state.js?v=20260716';
-import { buildCorsi } from './buildCorsi.js?v=20260716';
-
-export function corsiForYear() {
-  return buildCorsi().filter((c) => c.anno === state.year);
-}
-
--- src/utils/dom.js
+++
@@ -1,8 +0,0 @@
-export const $ = (id) => document.getElementById(id);
-
-export const escapeAttr = (s) => String(s ?? '').replace(/'/g, "\\'").replace(/"/g, '&quot;');
-
-export function setHTML(id, html) {
-  const el = $(id);
-  if (el) el.innerHTML = html;
-}
